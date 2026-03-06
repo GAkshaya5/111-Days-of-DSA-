99bd85b16d5b5e97429a2f8083d297b5811d0466
GAkshaya5 <akshayagundeti13@gmail.com>
2026-03-06 20:30:35 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,9 +1,7 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int i = m - 1;      // Pointer for the end of valid elements in nums1
-    int j = n - 1;      // Pointer for the end of nums2
-    int k = m + n - 1;  // Pointer for the last position in nums1
-
-    // While there are elements in both arrays
+    int i = m - 1;      
+    int j = n - 1;      
+    int k = m + n - 1;  
     while (i >= 0 && j >= 0) {
         if (nums1[i] > nums2[j]) {
             nums1[k--] = nums1[i--];
@@ -11,9 +9,6 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
             nums1[k--] = nums2[j--];
         }
     }
-
-    // If there are remaining elements in nums2, copy them
-    // (If elements remain in nums1, they are already in the right place)
     while (j >= 0) {
         nums1[k--] = nums2[j--];
     }
